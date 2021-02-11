d606729d36f410b1a780a3eb0cbe4305517fb545
minto0626 <daikituttiminto@gmail.com>
2021-02-11 23:49:45 +0900

    ゲームの進行を管理するスクリプトを作成

---
+++ Assets/Scripts/System/GameMaster.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    [SerializeField] GameObject playerPrefab;
+    GameObject playerObj;
+    [SerializeField] Vector3 setPlayerPos = new Vector3(0f, -3f, 0f);
+
+    private void Start()
+    {
+        CreatePlayer();
+    }
+
+    void CreatePlayer()
+    {
+        var player = Instantiate(playerPrefab, setPlayerPos, Quaternion.identity);
+    }
+
+    private void Update()
+    {
+        
+    }
+}
