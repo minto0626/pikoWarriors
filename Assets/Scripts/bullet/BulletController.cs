058ee80e58c325c12ac35620f0be2f6a6eed3c46
minto0626 <daikituttiminto@gmail.com>
2021-02-11 22:33:16 +0900

    弾が上方向に飛ぶように実装

---
+++ Assets/Scripts/bullet/BulletController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    float moveSpeed = 0.1f;
+
+    void Update()
+    {
+        gameObject.transform.Translate(0f, moveSpeed, 0f);
+    }
+}
