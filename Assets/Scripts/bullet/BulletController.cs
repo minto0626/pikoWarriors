a8f989085caa49d7565ddf72d5a3313ced9bf211
minto0626 <daikituttiminto@gmail.com>
2021-02-13 15:32:14 +0900

    初期化時にゲームマスターのインスタンスを取得

--- Assets/Scripts/bullet/BulletController.cs
+++ Assets/Scripts/bullet/BulletController.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 public class BulletController : MonoBehaviour
 {
     float moveSpeed = 0.1f;
+    GameMaster gm = null;
+
+    void Start()
+    {
+        Setup();
+    }
 
     void Update()
     {
         gameObject.transform.Translate(0f, moveSpeed, 0f);
+
+        Disappear();
+    }
+
+    void Setup()
+    {
+        gm = GameMaster.Instance;
+    }
+
     }
 }
