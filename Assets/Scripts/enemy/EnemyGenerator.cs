7311815b6a467dbb6c352ff7f9d41d1d6040f215
minto0626 <daikituttiminto@gmail.com>
2021-02-27 16:40:58 +0900

    敵のジェネレータを作成

---
+++ Assets/Scripts/enemy/EnemyGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : SingletonMonoBehaviour<EnemyGenerator>
+{
+    [SerializeField] GameObject[] generatePos;
+    GameObject enemyObj;
+
+    const int GENERATE_MAX = 3;
+
+    void Start()
+    {
+        if (CheckInstance())
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        enemyObj = MasterDataStore.Instance.GetObject(MasterDataStore.DataType.ENEMY);
+    }
+
+    public void Generate()
+    {
+        for (var index = 0; index < GENERATE_MAX; index++)
+        {
+            var obj = Instantiate(enemyObj, generatePos[index].transform.position, Quaternion.identity);
+        }
+    }
+}
