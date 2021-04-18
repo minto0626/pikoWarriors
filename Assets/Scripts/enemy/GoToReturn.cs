96eb9d70ba40321654ffb30003ae75f982d346b7
minto0626 <daikituttiminto@gmail.com>
2021-04-18 23:44:17 +0900

    敵の行動パターンを追加(行き来)

---
+++ Assets/Scripts/enemy/GoToReturn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 行って帰る
+/// </summary>
+public class GoToReturn : EnemyBase
+{
+    private void Start()
+    {
+        base.Setup();
+        movePos.y = base.moveSpeed;
+        StartCoroutine(Move());
+    }
+
+    /// <summary>
+    /// 移動
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Move()
+    {
+        while (true)
+        {
+            if (gameObject.transform.position.y < 0f) break;
+            transform.position += movePos;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(3f);
+
+        while (true)
+        {
+            if (base.Disappear()) break;
+            transform.position -= movePos;
+            yield return null;
+        }
+    }
+}
