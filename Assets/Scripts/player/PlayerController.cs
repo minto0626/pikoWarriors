4ba097a58bac3f9a8d5d8514c0d375b77d0e8201
minto0626 <daikituttiminto@gmail.com>
2021-05-08 00:53:38 +0900

    オーバーフロッシャーのような弾を撃てるようにする

--- Assets/Scripts/player/PlayerController.cs
+++ Assets/Scripts/player/PlayerController.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    /// <summary>
+    /// 弾の発射位置
+    /// </summary>
+    /// <returns></returns>
     Vector3 shotPoint = new Vector3(0f, 0.3f, 0f);
-
+    /// <summary>
+    /// プレイヤーの移動速度
+    /// </summary>
     float moveSpeed = 0.05f;
+
+    /// <summary>
+    /// 発射可能フラグ
+    /// </summary>
+    bool isShot = false;
+    /// <summary>
+    /// 1ショットの発射回数
+    /// </summary>
+    float SHOT_LIMIT = 4;
 
     private void Update()
     {
@@ -38,14 +53,51 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            OneShot();
+            // 弾を発射していなかったら撃てる
+            if (!isShot) 
+            {
+                StartCoroutine(OhuroShot());
+            }
         }
     }
 
     private void OneShot()
     {
         var bullet = ObjectPooler.Instance.GetObject();
-        bullet.transform.position = transform.localPosition + new Vector3(0f, 1f, 0f);
+        bullet.transform.position = transform.localPosition + shotPoint;
         CollisionManager.Instance.AddList(bullet.GetComponent<ObjectCollision>());
+    }
+
+    /// <summary>
+    /// オーバーフロッシャーのようなショット
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator OhuroShot()
+    {
+        // 撃った数のカウント
+        var shotCount = 0;
+        // 撃ち始めのタイマー
+        var shotTimer = 0f;
+        
+        isShot = true;
+
+        while(true)
+        {
+            if(shotCount == SHOT_LIMIT)
+            {
+                isShot = false;
+                yield break;
+            }
+
+            // 2fごとに弾を出す
+            if(shotTimer >= 0.1f * shotCount)
+            {
+                OneShot();
+                shotCount++;
+            }
+
+            shotTimer += Time.deltaTime;
+            yield return null;
+        }
     }
 }
