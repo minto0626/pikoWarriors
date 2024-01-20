9f280add82f07e6bbbfa0238e203ba0725f1b6e7
minto0626 <daikituttiminto@gmail.com>
2024-01-20 14:59:23 +0900

    弾の発射方向、速度を個別に与えられるように設定

--- Assets/Scripts/Weapon/Gun.cs
+++ Assets/Scripts/Weapon/Gun.cs
@@ -14,13 +14,23 @@ namespace Game
         /// 弾の発射位置
         /// </summary>
         readonly Vector3 SHOT_POINT = new(0f, -0.3f, 0f);
+        /// <summary>
+        /// 弾の発射方向
+        /// </summary>
+        readonly Vector3 SHOT_DIR = new Vector3(0f, -1f, 0f);
+        /// <summary>
+        /// 弾の速度
+        /// </summary>
+        readonly float SHOT_SPEED = 10f;
 
         public override void Attack() => Shot();
 
         void Shot()
         {
-            var bullet = GameMaster.Instance.CharacterManager.CreateChara(base.TeamObjType);
+            var bullet = GameMaster.Instance.CharacterManager.CreateChara(base.TeamObjType).GetComponent<BulletController>();
             bullet.transform.position = transform.position + SHOT_POINT;
+            bullet.SetMoveDir(SHOT_DIR);
+            bullet.SetMoveSpeed(SHOT_SPEED);
         }
     }
 }
